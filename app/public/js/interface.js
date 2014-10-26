1663c056416c062d9c22bc106fedefe705c1f19e
mala23 <mail@mala23.me>
2014-10-26 18:56:07 +0000

    make ajax work

--- app/public/js/interface.js
+++ app/public/js/interface.js
@@ -1,12 +1,42 @@
-$.getJSON('api/peeps', function(data){
-	data.reverse()
-	$.each(data, function(index, peep) {
+// $.getJSON('api/peeps', function(data){
+// 	data.reverse()
+// 	$.each(data, function(index, peep) {
+// 		var source = $("#peepTemplate").html();
+// 		var template = Handlebars.compile(source);
+// 		$('#peeps').append(template(peep));
+// 	});
+// });
+
+// $('#peep').on('click', function() {
+// 	$.post('/api/peeps/compose', {content: $('#content').val()});
+// });
+
+$(document).ready(function () {
+  // Load PEEPS data immediately
+  $.ajax({
+    url: '/api/peeps',
+    method: 'GET'
+  }).done(function (data) {
+  	data.reverse()
+    $.each(data, function (index, peep) {
 		var source = $("#peepTemplate").html();
 		var template = Handlebars.compile(source);
-		$('#peeps').append(template(peep));
-	});
+		$('#peeps').append(template(peep));    });
+  });
 });
 
-$('#peep').on('click', function() {
-	$.post('/api/peeps/compose', {content: $('#content').val()});
+	// Form to create a new Peep
+	$('#peep').on('click', function () {
+	  var data = {};
+
+	  // Get the data from the form elements
+	  data.content = $('#content').val();
+
+	  $.ajax({
+	    url: '/api/peeps/compose',
+	    method: 'POST',
+	    data: data
+	  }).done(function (response) {
+	    console.log(response);
+	  });
 });
